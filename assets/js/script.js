63df3c39c728468c09936ac3645d04ece67ab1ba
paul lovato <lovato.paul97@yahoo.com>
2022-08-11 18:05:55 -0400

    final steps and return function

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
         // Save text in local storage
         localStorage.setItem(timeBlock, textBlock);
     })
-})
+
 function timeTracker() {
     var timeNow = moment().hour();
 
@@ -36,5 +36,18 @@ function timeTracker() {
             $(this).addClass("future");
 
         }
-    })
+    });
 }
+$("#hour8 .description").val(localStorage.getItem("hour8"));
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
+
+timeTracker();
+});
